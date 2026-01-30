93d5652afad6156385deb9f873aeb427a4f203ee
EduTheGOAT <95358843+EduTheGOAT@users.noreply.github.com>
2026-01-30 18:11:49 +0100

    Add person follower node with LaserScan processing
    Implement a person following behavior using LaserScan data to control movement.

---
+++ code/Person Following V1.py
@@ -0,0 +1,72 @@
+# Copyright 2016 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rclpy
+from rclpy.node import Node
+
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+
+class PersonFollower(Node):
+
+    def __init__(self):
+        super().__init__('person_follower')
+        self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
+        self.subscription = self.create_subscription(
+            LaserScan,
+            '/scan',
+            self.listener_callback,
+            10)
+        self.subscription  
+
+    def listener_callback(self, input_msg):
+        angle_min = input_msg.angle_min
+        angle_max = input_msg.angle_max
+        angle_increment = input_msg.angle_increment
+        ranges = input_msg.ranges
+        #
+        # your code for computing vx, wz
+        #
+        valid_points = [(i, r) for i, r in enumerate(ranges) if r < float('inf')]
+
+        if len(valid_points) > 0:
+            closest_point = min(valid_points)
+            index_min, min_range = closest_point
+            angle_target = angle_min + (index_min * angle_increment)
+            
+            print(f"Objetivo: {min_range:.2f}m | Ángulo: {angle_target:.2f} rad | Índice: {index_min}")
+            wz = 0.5 * angle_target 
+
+            error_distancia = min_range - 0.4
+            vx = 0.2 * error_distancia 
+
+        else:
+            vx = 0.0
+            wz = 0.0
+        #
+        output_msg = Twist()
+        output_msg.linear.x = vx
+        output_msg.angular.z = wz
+        self.publisher_.publish(output_msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    person_follower = PersonFollower()
+    rclpy.spin(person_follower)
+    person_follower.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
